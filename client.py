5fca22801bcf4164c9b800ff0b33a4b322f717b3
MirkoAvramovic <mirkoavramovicsluzbeni@gmail.com>
2023-04-19 12:23:13 +0200

    request_replay_pattern

---
+++ client.py
@@ -0,0 +1,34 @@
+import pika
+import uuid
+
+def on_reply_message_recieved(ch, method, properties, body):
+    print(f"reply recieved: {body}")
+
+connection_parameters = pika.ConnectionParameters('localhost')
+
+connection = pika.BlockingConnection(connection_parameters)
+
+channel = connection.channel()
+
+reply_queue = channel.queue_declare(queue='', exclusive=True)
+
+channel.basic_consume(queue=reply_queue.method.queue, auto_ack=True,
+                      on_message_callback=on_reply_message_recieved)
+
+channel.queue_declare(queue='request-queue')
+
+message = "Can I request a reply?"
+
+cor_id = str(uuid.uuid4())
+
+print(f"Sending Request: {cor_id}")
+
+channel.basic_publish(exchange='', routing_key='request-queue', 
+                        properties=pika.BasicProperties(
+                            reply_to=reply_queue.method.queue,
+                            correlation_id=cor_id,),
+                        body=message)
+
+print(f"Starting Client")
+
+channel.start_consuming()
